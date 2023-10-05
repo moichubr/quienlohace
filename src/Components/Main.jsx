0577a5acffb53282468a2449a85161388cc7350f
moichubr <brunmoira@gmail.com>
2023-10-05 11:59:10 -0300

    first styles

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,50 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 import Constants from "expo-constants" //expo trae constantes ya declaradas que tienen default values para ciertas cosas
 import Searchbar from "./Searchbar"
 
 const Main = () => {
     return(
-    <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1}}>
-        <Text>QLH?</Text>
-        <Text>Quién lo hace?</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>QLH?</Text>
+        <Text style={styles.subtitle}>Quién lo hace?</Text>
         <Searchbar />
     </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flex: 1, //equivale al 100% del contenedor padre. necesario para que los hijos puedan expandirse
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 2,
+    backgroundImage: 'linear-gradient(0deg, rgb(255, 107, 0), rgb(55, 11, 82))',
+    },
+    title: {
+        color: '#FF9110',
+        fontSize: 80,
+        fontWeight: 'Light',
+        textShadowColor: '#000000',  // Color del sombreado
+        textShadowOffset: {       // Desplazamiento del sombreado
+            width: 1,
+            height: 1,
+    },
+    textShadowRadius: 2,
+    height: 102,
+    width: 227,
+    marginTop: 40,           // Margen superior de 40
+    marginHorizontal: 'auto', // Centra horizontalmente
+    marginBottom: 0,         // Margen inferior de 0   
+    letterSpacing: 3     
+        
+    },
+    subtitle: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
+})
 
 export default Main
--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,26 +1,75 @@
 import React from "react";
-import {View, Text, TextInput, TouchableHighlight, TouchableWithoutFeedback} from "react-native"
+import {View, Text, TextInput, TouchableHighlight, TouchableWithoutFeedback, StyleSheet} from "react-native"
 // import Rnepicture from "./RNEpicture";
 
 const Searchbar = () => {
 
     return(
-        <View>
-            <Text>Ingresa el R.N.E</Text>
-            <TextInput></TextInput>
-            <TouchableHighlight>
-                <Text>BUSCAR</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Ingresa el R.N.E</Text>
+            <TextInput style={styles.input} placeholder="02000167"></TextInput>
+            <TouchableHighlight style={styles.button}>
+                <Text style={styles.text}>BUSCAR</Text>
             </TouchableHighlight>
 
             <TouchableWithoutFeedback>
-                <Text>Dónde está el R.N.E en el producto?</Text>
+                <Text style={styles.firstanchor}>Dónde está el R.N.E en el producto?</Text>
             </TouchableWithoutFeedback>
 
             
             <TouchableWithoutFeedback>
-                <Text>Qué es el R.N.E?</Text>
+                <Text style={styles.secondanchor}>Qué es el R.N.E?</Text>
             </TouchableWithoutFeedback>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: '30%',
+        alignItems: 'center',
+        justifyContent: 'center',
+},
+title:{
+    color: '#FFFFFF',
+    fontSize: 16,
+
+},
+input: {
+    backgroundColor:'#FFFFFF',
+    width: 286,
+    height: 42,
+    margin: 8,
+    color: 'gray',
+    textAlign: 'center',
+    fontSize: 16,
+    paddingBottom: 2
+},
+button: {
+    backgroundColor: '#20C11D',
+    height: 33,
+    width: 130,
+    borderRadius: 6,
+    marginTop: 12
+},
+text: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginVertical: 'auto',
+    paddingBottom: 2
+ 
+},
+firstanchor: {
+    color: '#7289FE',
+    fontSize: 14,
+    marginTop: 24,
+    
+},
+secondanchor: {
+        color: '#7289FE',
+        fontSize: 14,
+        marginTop: 10,
+}
+})
 export default Searchbar
